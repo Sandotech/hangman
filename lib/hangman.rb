24ba1c82b657ba99a9a343e736f547d577871fd0
Diego Santos <dseosantos76@gmail.com>
2024-09-25 18:25:25 -0400

    🦺 Add: validation to input

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -3,6 +3,7 @@
 # Hangman game class
 class Hangman
   TURN_LIMIT = 9
+  USED_CHARACTERS = []
 
   attr_reader :actual_guess
 
@@ -19,13 +20,18 @@ class Hangman
       puts "Turns left: #{turns_left}"
       puts "Current state: #{hidden_word}"
 
-      print 'Enter a letter: '
-      guess = gets.chomp.downcase
+      guess = try_guess
 
       if word.include?(guess)
-
+        puts 'true'
       end
     end
+  end
+
+  def try_guess
+    print 'Enter a letter: '
+    guess = gets.chomp.downcase
+    return valid_character?(guess) ? save_character(guess) : try_guess
   end
 
   def print_game
@@ -34,8 +40,29 @@ class Hangman
 
   private
 
+  def valid_character?(character)
+    valid_length?(character) && character_used?(character)
+  end
+
+  def valid_length?(character)
+    nil_character?(character) && character.size == 1
+  end
+
+  def nil_character?(character)
+    !character.nil?
+  end
+
   def hide_word(word)
     word.split("").inject(String.new) { |string, val| string += "_" }
+  end
+
+  def save_character(character)
+    USED_CHARACTERS << character
+    character
+  end
+
+  def character_used?(character)
+    !USED_CHARACTERS.include? character
   end
 
   def select_word
@@ -45,4 +72,3 @@ class Hangman
 end
 
 game = Hangman.new
-p game.actual_guess
