a2c89395acba95b349b3186ed11daf3a690dfc4a
Diego Santos <dseosantos76@gmail.com>
2024-09-26 13:42:44 -0400

    ♻️ Refactor: linting game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -1,38 +1,38 @@
+# frozen_string_literal: true
+
 require_relative 'hangman'
 
+# Game class to play a new game or resume a previous game
 class Game
-
   def self.play
-    puts "Hi, would you like to load a previous game or start a new one?"
-    puts "Type 1 to load a previous game"
-    puts "Type 2 to start a new game"
+    puts 'Hi, would you like to load a previous game or start a new one?'
+    puts 'Type 1 to load a previous game'
+    puts 'Type 2 to start a new game'
     select_mode
   end
 
   def self.select_mode
     mode = chose_mode(gets.chomp)
     case mode
-    when "1"
+    when '1'
       load_previous_game
-    when "2"
+    when '2'
       start_new_game
     else
-      puts "Invalid selection, please try again"
+      puts 'Invalid selection, please try again'
     end
   end
 
-  private
-
   def self.chose_mode(selection)
-    until selection == "1" || selection == "2"
-      puts "Choose 1 or 2"
+    until %w[1 2].include?(selection)
+      puts 'Choose 1 or 2'
       selection = gets.chomp
     end
     selection
   end
 
   def self.start_new_game
-    hangman = Hangman::new
+    hangman = Hangman.new
     hangman.play
   end
 
@@ -46,7 +46,7 @@ class Game
   def self.select_game(games)
     selection = gets.chomp
     until selection.to_i.between?(1, games.size)
-      puts "Choose a valid game number"
+      puts 'Choose a valid game number'
       selection = gets.chomp
     end
     selection.to_i - 1
@@ -59,7 +59,7 @@ class Game
 
   def self.read_previous_game(game)
     serialized_game = File.binread("GAMES/#{game}")
-    game = Marshal.load(serialized_game)
+    Marshal.load(serialized_game)
   end
 
   def self.resume_game(game)
@@ -68,15 +68,15 @@ class Game
   end
 
   def self.load_games
-    games = Dir.children('GAMES').select { |file| file.include?('_data.marshal') }
+    Dir.children('GAMES').select { |file| file.include?('_data.marshal') }
   end
 
   def self.show_games(games)
-    puts "Previous Games:"
-    puts "--------------"
-    puts "Choose the game number to load"
+    puts 'Previous Games:'
+    puts '--------------'
+    puts 'Choose the game number to load'
     games.map.with_index do |game, index|
-      puts "#{index += 1}) #{game.gsub("_data.marshal", "")}"
+      puts "#{index + 1}) #{game.gsub('_data.marshal', '')}"
     end
   end
 end
